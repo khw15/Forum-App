66ff991d6c827b14360ccedd568678ebf679b81e
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2024-01-13 01:21:14 +0700

    add loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoadingBar from 'react-redux-loading-bar';
+
+function Loading() {
+  return (
+    <div className="w-full">
+      <LoadingBar />
+    </div>
+  );
+}
+
+export default Loading;
