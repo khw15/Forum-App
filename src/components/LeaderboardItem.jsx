1f2ee3c7c5541b1f3bdc2760f18bee31286630f4
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2024-01-13 01:16:09 +0700

    add Leaderboard components

---
+++ src/components/LeaderboardItem.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function LeaderboardItem({avatar, name, score}) {
+  return (
+    <div className="flex justify-between text-xl items-center">
+      <div className="flex items-center gap-3">
+        <img
+          src={avatar}
+          className="rounded-full"
+          alt="User's avatar"
+          width={50}
+          height={50}
+        />
+        <p>{name}</p>
+      </div>
+      <p>{score}</p>
+    </div>
+  );
+}
+
+LeaderboardItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+export default LeaderboardItem;
---
+++ src/components/LeaderboardList.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import LeaderboardItem from './LeaderboardItem';
+import {userProp} from '../utils/propsHelper';
+
+function LeaderboardList({leaderboards}) {
+  return (
+    <div className="flex flex-col gap-3">
+      <div className="flex justify-between text-xl">
+        <p>Pengguna</p>
+        <p>Skor</p>
+      </div>
+      <div className="flex flex-col gap-3">
+        {leaderboards.map((leaderboard) => (
+          <LeaderboardItem
+            key={leaderboard.user.id}
+            avatar={leaderboard.user.avatar}
+            name={leaderboard.user.name}
+            score={leaderboard.score}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+LeaderboardList.propTypes = {
+  leaderboards: PropTypes.arrayOf(
+      PropTypes.shape({
+        user: PropTypes.shape(userProp).isRequired,
+        score: PropTypes.number.isRequired,
+      }),
+  ).isRequired,
+};
+
+export default LeaderboardList;
