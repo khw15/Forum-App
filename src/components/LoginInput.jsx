3fd95d073a8f1f54e63b7b1a7bae57fe6a30d3ee
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2024-01-13 01:22:19 +0700

    add login & register component

---
+++ src/components/LoginInput.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import useInput from '../hooks/useInput';
+
+function LoginInput({login}) {
+  const [email, onEmailChange] = useInput('');
+  const [password, onPasswordChange] = useInput('');
+
+  return (
+    <form className="flex flex-col w-full gap-3">
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={onEmailChange}
+        className="border-2 border-gray-400 rounded-md p-1"
+        required
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={onPasswordChange}
+        className="border-2 border-gray-400 rounded-md p-1"
+        required
+      />
+      <button
+        type="submit"
+        onClick={() => login({email, password})}
+        className="bg-slate-700 p-1 rounded-md text-white"
+      >
+        Login
+      </button>
+    </form>
+  );
+}
+
+LoginInput.propTypes = {
+  login: PropTypes.func.isRequired,
+};
+
+export default LoginInput;
---
+++ src/components/RegisterInput.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import useInput from '../hooks/useInput';
+
+function RegisterInput({register}) {
+  const [name, onNameChange] = useInput('');
+  const [email, onEmailChange] = useInput('');
+  const [password, onPasswordChange] = useInput('');
+
+  return (
+    <form className="flex flex-col w-full gap-3">
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={onNameChange}
+        className="border-2 border-gray-400 rounded-md p-1"
+        required
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={onEmailChange}
+        className="border-2 border-gray-400 rounded-md p-1"
+        required
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={onPasswordChange}
+        className="border-2 border-gray-400 rounded-md p-1"
+        required
+      />
+      <button
+        type="submit"
+        onClick={() => register({name, email, password})}
+        className="bg-slate-700 p-1 rounded-md text-white"
+      >
+        Register
+      </button>
+    </form>
+  );
+}
+
+RegisterInput.propTypes = {
+  register: PropTypes.func.isRequired,
+};
+
+export default RegisterInput;
